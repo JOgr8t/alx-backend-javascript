ff0d344d864bed1e8c871be514f4c01b7c1f2eee
JOgr8t <jonathanKingoche@gmail.com>
2023-03-30 18:26:48 -0700

    a set from an array

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Adayehi Jonathan Oche <https://github.com/JOgr8t>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
